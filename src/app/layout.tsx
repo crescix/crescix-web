02521633d66b92b31930a1d3b8c1e2fa2db6e46a
lucas <lucasmelo8328@gmail.com>
2026-01-19 22:39:42 -0300

    feat: cores, componentes next, fontes setadas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Plus_Jakarta_Sans, Poppins } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,19 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta-sans",
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ['400', '700', '800', '900'],
+  subsets: ["latin"],
+});
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
